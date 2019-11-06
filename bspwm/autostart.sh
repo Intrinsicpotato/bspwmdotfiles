89ba17365c581727ae67dfb7a6a2140da29c155a
Intrinsicpotato <rhonnin@protonmail.com>
2019-11-06 15:16:43 -0700

    Added dot files

---
+++ bspwm/autostart.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+
+#xrandr --output DP-0 --primary --mode 1920x1080 --rate 119.98 --pos 960x0 --output DP-4 --mode 3840x2160 --rate 60 --pos 0x1080
+xrandr --output DP-0 --mode 1920x1080 --rate 119.98 --primary --pos 960x0 --output DP-4 --pos 0x1080
+#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+
+$HOME/.config/polybar/launch.sh &
+
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+#Some ways to set your wallpaper besides variety or nitrogen
+#feh --bg-scale ~/.config/bspwm/wall.png &
+#feh --bg-scale /usr/share/backgrounds/arcolinux/escape-to-italy-1920x1200.jpg &
+feh --randomize --bg-fill ~/Pictures/.wallpapers/a/*.jpg ~/Pictures/.wallpapers/a/*.jpg
+#feh --randomize --bg-fill ~/Képek/*
+#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
+
+xsetroot -cursor_name left_ptr &
+sxhkd &
+
+#conky -c $HOME/.config/bspwm/system-overview &
+#run variety &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+blueberry-tray &
+compton --config $HOME/.config/bspwm/compton.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#nitrogen --restore &
+#run caffeine &
+#run vivaldi-stable &
+#run firefox &
+#run thunar &
+#run dropbox &
+#run insync start &
+#run discord &
+#run spotify &
+#run atom &
